958abeceed89fab236453a1b73beacfacfcb4b98
dbircsak <dbircsak@users.noreply.github.com>
2025-09-20 09:31:42 -0700

    Adding support files

---
+++ freeze_orig/freeze.bat
@@ -0,0 +1,9 @@
+@echo off
+set Q2DIR=C:\Quake2
+
+copy /Y "gamex86.dll" "%Q2DIR%\freeze\"
+copy /Y "autoexec.cfg" "%Q2DIR%\freeze\"
+copy /Y "freeze.cfg" "%Q2DIR%\freeze\"
+copy /Y "freeze.ini" "%Q2DIR%\freeze\"
+copy /Y "pak2.pak" "%Q2DIR%\freeze\"
+start "" /D "%Q2DIR%" quake2.exe +set game freeze +set developer 1 +connect 127.0.0.1
